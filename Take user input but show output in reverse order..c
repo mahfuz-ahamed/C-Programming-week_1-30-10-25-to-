3e4685c2f4d97f59a5e5ebf4c20c2ecf0707351f
Mahfuz Ahamed Pallab <mahfuzahamed5737@gmail.com>
2025-11-06 12:28:53 +0600

    Add files via upload

---
+++ Take user input but show output in reverse order..c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+
+int n;
+int arr[n],i;
+scanf("%d",&n);
+for(i=0;i<n;i++){
+    
+
+scanf("%d",&arr[i]);
+
+}
+for(i=n-1;i>=0;i--){
+    printf(" arry of %d %d\n",i,arr[i]);
+}
+
+    return 0;
+}
